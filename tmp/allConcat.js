593b8144ed09ca1ed52a201d443da121a4426f64
Javier Jimenez and Mark Helt <javier@>
2017-10-11 15:53:31 -0700

    add keyboard to the game

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -21,13 +21,13 @@ $(document).ready(function() {
     let array1 = [  ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],
                     ["w"],["w"],["w"],["l"],["l"],["l"],["w"],["w"],["w"],["w"],["w"],["w"],
                     ["l"],["l"],["l"],["l"],["w"],["l"],["w"],["w"],["w"],["w"],["w"],["w"],
-                    ["l"],["l"],["l"],["l"],["l"],["l"],["l"],["w"],["w"],["w"],["w"],["w"],
-                    ["w"],["l"],["w"],["w"],["w"],["w"],["w"],["l"],["l"],["w"],["w"],["w"],
-                    ["l"],["l"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],
-                    ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["l"],["w"],
-                    ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],
-                    ["l"],["l"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],
-                    ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"],["w"]
+                    ["l"],["l"],["l"],["l"],["l"],["l"],["l"],["l"],["w"],["w"],["w"],["w"],
+                    ["w"],["l"],["w"],["w"],["w"],["w"],["w"],["l"],["l"],["l"],["w"],["w"],
+                    ["l"],["l"],["w"],["w"],["w"],["w"],["w"],["l"],["w"],["l"],["l"],["w"],
+                    ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["l"],["w"],["w"],["l"],["w"],
+                    ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["l"],["w"],["w"],["l"],["l"],
+                    ["l"],["l"],["w"],["w"],["w"],["w"],["w"],["l"],["w"],["w"],["w"],["l"],
+                    ["w"],["w"],["w"],["w"],["w"],["w"],["w"],["l"],["w"],["w"],["w"],["l"]
                 ];
 
     let type = $("#character_type").val();
@@ -39,7 +39,7 @@ $(document).ready(function() {
     $("#game").show();
 
 
-    let current_level = level1
+    let current_level = level1;
 
     for(let sqr = 0; sqr < 120; sqr++){
       if (current_level.grid[sqr][0] === "w"){
@@ -50,15 +50,41 @@ $(document).ready(function() {
       }
     }
     // on click
-    var last_id = player.position;
-    $(`#${last_id}`).html(`<img src='./${player.type}.png'>`);
-    $(".sqr").click(function(e){
-      let current_id = parseInt($(e.currentTarget).attr('id'));
+    // var last_id = player.position;
+    // $(`#${last_id}`).html(`<img src='./${player.type}.png'>`);
+    // move player by click
+    // $(".sqr").click(function(e){
+    //   let current_id = parseInt($(e.currentTarget).attr('id'));
+    //
+    //   if((current_level.grid[`${current_id}`][0] === "l") && (((last_id-1) === current_id) || ((last_id+1) === current_id) || ((last_id-12) === current_id)|| ((last_id+12) === current_id))){
+    //     $(e.currentTarget).html(`<img src='./${player.type}.png'>`);
+    //     $(`#${last_id}`).html("");
+    //     last_id = current_id;
+    //   }
+    // });
 
-      if((current_level.grid[`${current_id}`][0] === "l") && (((last_id-1) === current_id) || ((last_id+1) === current_id) || ((last_id-12) === current_id)|| ((last_id+12) === current_id))){
-        $(e.currentTarget).html(`<img src='./${player.type}.png'>`);
-        $(`#${last_id}`).html("");
-        last_id = current_id;
+    var current_id = player.position;
+    $(`#${current_id}`).html(`<img src='./${player.type}.png'>`);
+    $(document).keydown(function(e){
+      if((e.key === "ArrowRight") &&(current_level.grid[`${current_id + 1}`][0] === "l")){
+        $(`#${current_id + 1}`).html(`<img src='./${player.type}.png'>`);
+        $(`#${current_id}`).html("");
+        current_id += 1;
+      }
+      if((e.key === "ArrowLeft") &&(current_level.grid[`${current_id - 1}`][0] === "l")){
+        $(`#${current_id - 1}`).html(`<img src='./${player.type}.png'>`);
+        $(`#${current_id}`).html("");
+        current_id -= 1;
+      }
+      if((e.key === "ArrowDown") &&(current_level.grid[`${current_id + 12}`][0] === "l")){
+        $(`#${current_id + 12}`).html(`<img src='./${player.type}.png'>`);
+        $(`#${current_id}`).html("");
+        current_id += 12;
+      }
+      if((e.key === "ArrowUp") &&(current_level.grid[`${current_id - 12}`][0] === "l")){
+        $(`#${current_id - 12}`).html(`<img src='./${player.type}.png'>`);
+        $(`#${current_id}`).html("");
+        current_id -= 12;
       }
     });
   });
